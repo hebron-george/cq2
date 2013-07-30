503ae530f4cffcd8cde8f8cee07d3ce2d718917f
Hebron George <hebrontgeorge@gmail.com>
2013-07-30 18:16:22 -0400

    Added constants for page names and visitorStats table.

--- config.php
+++ config.php
@@ -5,13 +5,20 @@
 		const db_user = 'vashy_one';
 		const db_pass = '7_qq}QnpuPe!';
 		const db_name = 'vashy_cq2';
+
 		const victims_table = 'victims';
 		const reveals_table = 'reveals';
+		const visitor_stats = 'visitorStats';
 
 		const SA = 'SA';
 		const META = 'META';
 		const DOPPLE = 'DOPPLE';
 		const SUFFO = 'SUFFO';
 		const JINX = 'JINX';
+
+		const REVEAL_INDEX = 'reveals/index.php';
+		const REVEAL_FIND = 'reveals/findReveal.php';
+
+		const CURSE_ADD = 'curses/index.html';
 	}
 ?>
