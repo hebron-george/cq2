6935b714af1cfde498a4dd7c9d4fd932477d6105
Hebron George <hebrontgeorge@gmail.com>
2013-08-01 15:47:24 -0400

    Menu created for navigation.

---
+++ menu.php
@@ -0,0 +1,13 @@
+<div id="container">
+	<div id="bannerImg">
+	</div>
+	<div class="menu-item">
+		<a href="http://www.hebrongeorge.com/cq2/reveals">Reveals</a>
+	</div>
+	<div class="menu-item">
+		<a href="http://www.hebrongeorge.com/cq2/curses">Curses</a>
+	</div>
+	<div class="menu-item">
+		<a href="http://www.hebrongeorge.com/">Home</a>
+	</div>
+</div>
