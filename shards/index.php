906432676feb2be61163a408ed6362b5f4bf27eb
Hebron George <hebrontgeorge@gmail.com>
2013-08-04 01:19:59 -0400

    Removes all input from the current user to update it with their new input.

--- shards/index.php
+++ shards/index.php
@@ -47,6 +47,43 @@
 	<body>
 		<?
 			include("../menu.php");
+
+			if (isset($_POST['user']) && strlen($_POST['user']) > 0 && isset($_POST['shards']) && strlen($_POST['shards']) > 0)
+			{
+				$user = trim(htmlentities($_POST['user'], ENT_QUOTES));
+				$shards = trim(htmlentities($_POST['shards']), ENT_QUOTES);
+
+				$stmt = $dbh->prepare("DELETE FROM ".Config::shards_table." WHERE user=?");
+				$stmt->bindParam(1, $user);
+				$stmt->execute();
+
+				$lines = explode(PHP_EOL, $shards);
+
+				$pattern = "(.+),.+(d+)";
+				foreach($lines as $i)
+				{
+					preg_match($pattern, $i, $m);
+
+					$shard = $m[1];
+					echo $shard."<br>";
+					//$stmt = $dbh->prepare("INSERT INTO ".Config::shards_table." (user, shard, amount, submissionDate) VALUES (?, ?, ?, NOW())")
+				}
+			}
 		?>
+		<center>
+			<form action="" method="POST">
+				<h2>Submit Your Shards</h2>
+				<br>
+				Mage:
+				<br>
+				<input id="user" class="textbox" name="user" type="text"></input>
+				<br>
+				Shards:
+				<br>
+				<textarea id="shards" class="textbox" rows="25" cols="90" name="shards" type="text"></textarea>
+				<br>
+				<input type="submit"></input>
+			</form>
+		</center>
 	</body>
 </htm>
