9934800a798234e3c7de84cd9d6560f2568876b5
Hebron George <hebrontgeorge@gmail.com>
2013-08-04 00:29:58 -0400

    Added a link to shards page to the menu.

--- menu.php
+++ menu.php
@@ -2,6 +2,9 @@
 	<div id="bannerImg">
 	</div>
 	<div class="menu-item">
+		<a href="http://www.hebrongeorge.com/cq2/shards">Shards</a>
+	</div>
+	<div class="menu-item">
 		<a href="http://www.hebrongeorge.com/cq2/reveals">Reveals</a>
 	</div>
 	<div class="menu-item">
