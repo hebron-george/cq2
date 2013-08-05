3ea10f101c4a8d313a852f368e0755f58ff4c955
Hebron George <hebrontgeorge@gmail.com>
2013-08-04 21:53:13 -0400

    Created the cq2/ homepage.

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Castle Quest 2 Tools</title>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+
+	<body>
+		<?
+			include('menu.php');
+		?>
+	</body>
+</html>
