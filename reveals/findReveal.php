a76ad6faef40599c7f814475b88ad6c41cec96c4
Hebron George <hebrontgeorge@gmail.com>
2013-08-10 11:47:08 -0400

    Reveals can now be filtered by User name, User level or a search term within their list.

--- reveals/findReveal.php
+++ reveals/findReveal.php
@@ -52,16 +52,137 @@
 			include("../menu.php");
 		?>
 		<form action="findReveal.php" method="GET">
+			<h2>Filters</h2>
 			User:
-			<input type="text" id="user" name="user"></input> [OR]
+			<input type="text" id="user" name="user"></input> <br>
 			Level:
-			<input type="text" id="level" name="level"></input>
+			<input type="text" id="level" name="level"></input> <br>
+			List Search: 
+			<input type="text" id="crit" name="crit"></input>
 			<br>
 			<input type="submit"></input>
 		</form>
 		<div id="list">
 				<?
-					if (isset($_GET['user']) && $_GET['user'] !== "")
+				    $currentDate = date('Y-m-d H:i:s');
+					if (isset($_GET['user']) && $_GET['user'] !== "" && isset($_GET['level']) && $_GET['level'] !== "" && isset($_GET['crit']) && $_GET['crit'] !== "")
+					{
+						$user = htmlentities($_GET['user'], ENT_QUOTES);
+						$level = htmlentities($_GET['level'], ENT_QUOTES);
+						$crit = htmlentities($_GET['crit'], ENT_QUOTES);
+
+						$stmt = $dbh->prepare("SELECT list, submissionDate, userLevel FROM ".Config::reveals_table." WHERE user = ? AND userLevel = ? AND list LIKE ?");
+
+						$stmt->bindParam(1, $user);
+						$stmt->bindParam(2, $level);
+						$crit = '%'.$crit.'%';
+						$stmt->bindParam(3, $crit);
+
+						$stmt->execute();
+
+						$result = $stmt->fetchAll();
+						if (!isset($result[0]['list']) || $result[0]['list'] == "")
+							echo "<h2>There is no result for those search parameters.</h2>";
+						else
+						{
+							$list = $result[0]['list'];
+						    $submissionDate = $result[0]['submissionDate'];
+						    $userLevel = $result[0]['userLevel'];
+					    	$days = floor((strtotime($currentDate) - strtotime($submissionDate))/(60*60*24));
+							echo "<h2>".html_entity_decode($user)." - ".$userLevel."</h2>This reveal is $days days old.<br>";
+							echo nl2br(htmlspecialchars($list, ENT_QUOTES, 'UTF-8'));
+
+						}
+					}
+					else if (isset($_GET['user']) && $_GET['user'] !== "" && isset($_GET['level']) && $_GET['level'] !== "")
+					{
+						$user = htmlentities($_GET['user'], ENT_QUOTES);
+						$level = htmlentities($_GET['level'], ENT_QUOTES);
+
+						$stmt = $dbh->prepare("SELECT list, submissionDate, userLevel FROM ".Config::reveals_table." WHERE user = ? AND userLevel = ?");
+
+						$stmt->bindParam(1, $user);
+						$stmt->bindParam(2, $level);
+
+						$stmt->execute();
+						$result = $stmt->fetchAll();
+					    $submissionDate = $result[0]['submissionDate'];
+					    $userLevel = $result[0]['userLevel'];
+
+					    if (isset($result[0][0]))
+					    {
+					    	$days = floor((strtotime($currentDate) - strtotime($submissionDate))/(60*60*24));
+							echo "<h2>".html_entity_decode($user)." - ".$userLevel."</h2>This reveal is $days days old.<br>";
+							echo nl2br(htmlspecialchars($result[0]['list'], ENT_QUOTES, 'UTF-8'));
+						}
+						else
+						{
+							echo "<h2>There is no result for user: ".$user." at level ".$level."</h2>";
+						}
+
+					}
+					else if (isset($_GET['user']) && $_GET['user'] !== "" && isset($_GET['crit']) && $_GET['crit'] !== "")
+					{
+						$user = htmlentities($_GET['user'], ENT_QUOTES);
+						$crit = htmlentities($_GET['crit'], ENT_QUOTES);
+
+						$stmt = $dbh->prepare("SELECT list, submissionDate, userLevel FROM ".Config::reveals_table." WHERE user = ? AND list LIKE ?");
+
+						$stmt->bindParam(1, $user);
+						$crit = '%'.$crit.'%';
+						$stmt->bindParam(2, $crit);
+
+						$stmt->execute();
+						$result = $stmt->fetchAll();
+					    $submissionDate = $result[0]['submissionDate'];
+					    $userLevel = $result[0]['userLevel'];
+
+					    if (isset($submissionDate) && isset($result[0][0]))
+					    {
+					    	$days = floor((strtotime($currentDate) - strtotime($submissionDate))/(60*60*24));
+							echo "<h2>".html_entity_decode($user)." - ".$userLevel."</h2>This reveal is $days days old.<br>";
+							echo nl2br(htmlspecialchars($result[0]['list'], ENT_QUOTES, 'UTF-8'));
+						}
+						else
+						{
+							echo "<h2>There is no result for user: ".htmlspecialchars($user)." and search pattern: ".htmlspecialchars($crit)."</h2>";
+						}
+					}
+					else if (isset($_GET['level']) && $_GET['level'] !== "" && isset($_GET['crit']) && $_GET['crit'] !== "")
+					{
+						$level = htmlentities($_GET['level'], ENT_QUOTES);
+						$crit = htmlentities($_GET['crit'], ENT_QUOTES);
+
+						$stmt = $dbh->prepare("SELECT user, submissionDate FROM ".Config::reveals_table." WHERE userLevel = ? AND list LIKE ?");
+
+						$stmt->bindParam(1, $level);
+						$crit = '%'.$crit.'%';
+						$stmt->bindParam(2, $crit);
+
+						$stmt->execute();
+						$result = $stmt->fetchAll();
+					    $submissionDate = $result[0]['submissionDate'];
+
+					    if (count($result) > 0)
+					    {
+					    	echo '<h2>Users</h2>';
+					    	for ($i = 0; $i < count($result); $i++)
+						    {
+							    if (isset($submissionDate) && isset($result[$i]['user']))
+							    {
+							    	$days = floor((strtotime($currentDate) - strtotime($submissionDate))/(60*60*24));
+							    	$user = $result[$i]['user'];
+									echo '<a href="findReveal.php?user='.htmlspecialchars($user).'">'.$user.'</a> - '.$days.' days old<br>';
+								}
+						    }
+						}
+						else
+						{
+							echo '<h2>There were no users found with level: '.htmlspecialchars($level).' and search pattern: '.htmlspecialchars($crit).'</h2>';
+						}
+
+					}
+					else if (isset($_GET['user']) && $_GET['user'] !== "")
 					{
 						$user = htmlentities($_GET['user'], ENT_QUOTES);
 
@@ -72,7 +193,6 @@
 						$stmt->execute();
 
 						$result = $stmt->fetchAll();
-					    $currentDate = date('Y-m-d H:i:s');
 					    $submissionDate = $result[0]['submissionDate'];
 					    $userLevel = $result[0]['userLevel'];
 
@@ -89,7 +209,56 @@
 					}
 					else if (isset($_GET['level']) && $_GET['level'] !== "")
 					{
+						$level = htmlentities($_GET['level'], ENT_QUOTES);
 
+						$stmt = $dbh->prepare("SELECT user, submissionDate, userLevel FROM ".Config::reveals_table." WHERE userLevel = ?");
+
+						$stmt->bindParam(1, $level);
+
+						$stmt->execute();
+						$result = $stmt->fetchAll();
+
+						if (count($result) > 0)
+						{
+							echo '<h2>Potential Targets</h2>';
+							for ($i = 0; $i < count($result); $i++)
+							{
+					   			$submissionDate = $result[$i]['submissionDate'];
+					    		$days = floor((strtotime($currentDate) - strtotime($submissionDate))/(60*60*24));
+								echo '<a href="findReveal.php?user='.$result[$i]['user'].'">'.$result[$i]['user'].' - Level '.$result[$i]['userLevel'].' - '.$days.' old<br>';								
+							}
+						}
+						else
+						{
+							echo '<h2>There were no users found for level: '.$level.'</h2>';
+						}
+					}
+					else if (isset($_GET['crit']) && $_GET['crit'] !== "")
+					{
+						$crit = htmlentities($_GET['crit'], ENT_QUOTES);
+
+						$stmt = $dbh->prepare("SELECT user, submissionDate, userLevel FROM ".Config::reveals_table." WHERE list LIKE ? ORDER BY id DESC");
+
+						$crit = '%'.$crit.'%';
+						$stmt->bindParam(1, $crit);
+
+						$stmt->execute();
+						$result = $stmt->fetchAll();
+
+						if (count($result) > 0)
+						{
+							echo '<h2>Potential Targets</h2>';
+							for ($i = 0; $i < count($result); $i++)
+							{
+					   			$submissionDate = $result[$i]['submissionDate'];
+					    		$days = floor((strtotime($currentDate) - strtotime($submissionDate))/(60*60*24));
+								echo '<a href="findReveal.php?user='.$result[$i]['user'].'">'.$result[$i]['user'].' - Level '.$result[$i]['userLevel'].' - '.$days.' old<br>';
+							}
+						}
+						else
+						{
+							echo '<h2>No results were return with your search critera</h2>';
+						}
 					}
 
 				?>
