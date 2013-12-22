f57a6dc1079a27d38727fe8e3540c1a364e93be4
Hebron George <hebrontgeorge@gmail.com>
2013-12-22 03:01:36 -0500

    Moved some common session related functions in here.

---
+++ session_functions.php
@@ -0,0 +1,23 @@
+<?
+
+	session_start();
+	
+	function isLoggedIn()
+	{
+	    if(isset($_SESSION['valid']) && $_SESSION['valid'])
+	        return true;
+	    return false;
+	}
+
+	function logout()
+	{
+	    $_SESSION = array(); //destroy all of the session variables
+	    session_destroy();
+	}
+
+	function validateUser($username)
+	{
+		$_SESSION['valid'] = 1;
+		$_SESSION['username'] = $username;
+	}
+?>
